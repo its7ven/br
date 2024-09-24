0dce5e8f9a05e87b20db029fe71f9e259b9c5164
MatheusFurtado <matheusreisfurt@gmail.com>
2024-09-24 20:01:11 -0300

    commit

---
+++ dados.js
@@ -0,0 +1,24 @@
+let dados = [
+    "Vasco da Gama",
+    "Botafogo",
+    "Palmeiras",
+    "Atlético Mineiro",
+    "Juventude",
+    "Grêmio",
+    "Fortaleza",
+    "São Paulo",
+    "Flamengo",
+    "Vitória",
+    "Red Bull Bragantino ",
+    "Cruzeiro",
+    "Corinthians",
+    "Bahia",
+    "Criciúma",
+    "Fluminense",
+    "Atlético Goianiense",
+    "Cuiabá",
+    "Athletico Paranaense",
+    "Internacional",
+    
+];
+
